0ea469f76ef543807982087c315cc938317b7035
yasuladin <yasuhito.sakamoto@gmail.com>
2024-11-08 12:57:15 +0900

    ３章まで完了

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,71 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Image from 'next/image';
+import styles from './page.module.css';
+
+import NewsList from './_components/NewsList';
+import ButtonLink from './_components/ButtonLink';
+import type { News } from '@/app/_lib/microcms';
+
+const data: {
+  contents: News[];
+} = {
+  contents: [
+    {
+      id: '1',
+      title: '渋谷にオフィスを移転しました',
+      category: {
+        name: '更新情報',
+      },
+      publishedAt: '2023/05/19',
+      createdAt: '2023/05/19',
+    },
+    {
+      id: '2',
+      title: '当社CEOが業界リーダーTOP30に選出されました',
+      category: {
+        name: '更新情報',
+      },
+      publishedAt: '2023/05/19',
+      createdAt: '2023/05/19',
+    },
+    {
+      id: '3',
+      title: 'テストの記事です',
+      category: {
+        name: '更新情報',
+      },
+      publishedAt: '2023/04/19',
+      createdAt: '2023/04/19',
+    },
+  ],
+};
 
 export default function Home() {
+  const sliceData = data.contents.slice(0, 2);
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
+    <>
+      <section className={styles.top}>
+        <div>
+          <h1 className={styles.title}>テクノロジーの力で世界を変える</h1>
+          <p className={styles.description}>
+            私達は市場をリードしているグローバルテックカンパニーです。
+          </p>
+        </div>
         <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
+          src="/img-mv.jpg"
+          alt=""
+          className={styles.bgimg}
+          width={4000}
+          height={1200}
           priority
         />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+      </section>
+      <section className={styles.news}>
+        <h2 className={styles.newsTitle}>News</h2>
+        <NewsList news={sliceData} />
+        <div className={styles.newsLink}>
+          <ButtonLink href="/news">もっとみる</ButtonLink>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </section>
+    </>
   );
 }
