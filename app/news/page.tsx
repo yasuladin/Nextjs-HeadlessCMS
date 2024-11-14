15e720f7268e360d59d2470039f27280563686b9
yasuladin <yasuhito.sakamoto@gmail.com>
2024-11-14 12:18:26 +0900

    ７章まで完了

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,7 +1,18 @@
 import { getNewsList } from '@/app/_lib/microcms';
 import NewsList from '@/app/_components/NewsList';
+import Pagination from '../_components/Pagination';
+import SearchField from '../_components/SearchField';
+import { NEWS_LIST_LIMIT } from '@/app/_constants';
 
 export default async function Page() {
-  const { contents: news } = await getNewsList();
-  return <NewsList news={news} />;
+  const { contents: news, totalCount } = await getNewsList({
+    limit: NEWS_LIST_LIMIT,
+  });
+  return (
+    <>
+      <SearchField />
+      <NewsList news={news} />
+      <Pagination totalCount={totalCount} />
+    </>
+  );
 }
---
+++ app/news/search/page.tsx
@@ -0,0 +1,25 @@
+import { getNewsList } from '@/app/_lib/microcms';
+import { NEWS_LIST_LIMIT } from '@/app/_constants';
+import NewsList from '@/app/_components/NewsList';
+import SearchField from '@/app/_components/SearchField';
+// import styles from './page.module.css';
+
+type Props = {
+  searchParams: {
+    q?: string;
+  };
+};
+
+export default async function Page({ searchParams }: Props) {
+  const { contents: news } = await getNewsList({
+    limit: NEWS_LIST_LIMIT,
+    q: searchParams.q,
+  });
+
+  return (
+    <>
+      <SearchField />
+      <NewsList news={news} />
+    </>
+  );
+}
